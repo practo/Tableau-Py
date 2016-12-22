08d8cd1c0edaaa70d5f629c568ed1b44c02ded38
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-12-22 16:36:29 +0530

    Reorganizes tableausdk links in proper dictionary in setup.py

--- setup.py
+++ setup.py
@@ -13,18 +13,22 @@ tableau_sdk = None
 system = platform.system()
 architecture = platform.architecture()[0]
 
-if architecture == '64bit':
-    if system == 'Darwin':
-        tableau_sdk = 'https://downloads.tableau.com/tssoftware/Tableau-SDK-Python-OSX-64Bit-10-1-1.tar.gz'
-    elif system == 'Windows':
-        tableau_sdk = 'https://downloads.tableau.com/tssoftware/Tableau-SDK-Python-Win-64Bit-10-1-1.zip'
-    elif system == 'Linux':
-        tableau_sdk = 'https://downloads.tableau.com/tssoftware/Tableau-SDK-Python-Linux-64Bit-10-1-1.tar.gz'
-elif architecture == '32bit':
-    if system == 'Windows':
-        tableau_sdk = 'https://downloads.tableau.com/tssoftware/Tableau-SDK-C-Java-32Bit-10-1-1.zip'
-    elif system == 'Linux':
-        tableau_sdk = 'https://downloads.tableau.com/tssoftware/Tableau-SDK-Python-Linux-32Bit-10-1-1.tar.gz'
+_tableausdk_url_map = {
+    '64bit': {
+        'Darwin': 'https://downloads.tableau.com/tssoftware/Tableau-SDK-Python-OSX-64Bit-10-1-1.tar.gz',
+        'Windows': 'https://downloads.tableau.com/tssoftware/Tableau-SDK-Python-Win-64Bit-10-1-1.zip',
+        'Linux': 'https://downloads.tableau.com/tssoftware/Tableau-SDK-Python-Linux-64Bit-10-1-1.tar.gz',
+    },
+    '32bit': {
+        'Windows': 'https://downloads.tableau.com/tssoftware/Tableau-SDK-C-Java-32Bit-10-1-1.zip',
+        'Linux': 'https://downloads.tableau.com/tssoftware/Tableau-SDK-Python-Linux-32Bit-10-1-1.tar.gz',
+    }
+}
+
+tableau_architecture_url_map = _tableausdk_url_map.get(architecture)
+
+if tableau_architecture_url_map is not None:
+    tableau_sdk = tableau_architecture_url_map.get(system)
 
 if tableau_sdk is None:
     raise Exception('Not compatible system')
