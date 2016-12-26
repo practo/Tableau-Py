7c22a3df71fa93a37d9b3f79d788b91a9f86c93b
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-12-26 12:50:09 +0530

    Places new_extract.close() and table_definition.close() form finally to try, in order to track their expceptions if any

--- auto_extract/cli.py
+++ auto_extract/cli.py
@@ -118,17 +118,15 @@ def _generate_extract(tds_file_name, tde_file_name):
         table_definition = tds_reader.define_table()
 
         new_extract.addTable('Extract', tableDefinition=table_definition)
+
+        new_extract.close()
+        table_definition.close()
+
         result['status'] = _status.SUCCESS
     except (IOError, OSError) as err:
         result['msg'] = str(err)
     except TableauException:
         result['msg'] = GetLastErrorMessage()
-    finally:
-        if table_definition is not None:
-            table_definition.close()
-
-        # Close the extract in order to save the .tde file and clean up resources
-        new_extract.close()
 
     return result
 
