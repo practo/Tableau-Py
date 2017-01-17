87876d5fc3b38162c70f96522e0d865f384a7712
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2017-01-17 14:27:38 +0530

    Adds writer package and WriterException

---
+++ auto_extract/writers/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""This module defines tableau writer classes and exceptions
+
+Exceptions:
+* WriterException
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from auto_extract.writers.exceptions import WriterException
+
+__all__ = [
+    'WriterException',
+]
