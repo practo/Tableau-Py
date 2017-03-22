1ed800bbc4cc65ced001fe197e6b56c585b67c5c
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2017-03-22 13:06:54 +0530

    Adds exceptions required by content handler

--- auto_extract/content_handlers/exceptions.py
+++ auto_extract/content_handlers/exceptions.py
@@ -5,7 +5,40 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from auto_extract.exceptions import TableauPyException
 
-class ContentHandlerException(Exception):
+
+class ContentHandlerException(TableauPyException):
     """raised when an exception is thrown by a ContentHandlers"""
-    pass
+
+    _message_template = 'An error occurred with ContentHandler'
+
+
+class UnexpectedCount(ContentHandlerException, AssertionError):
+    """raised when an assertion of element count fails after parsing"""
+
+    _message_template = 'expected count of {} to be {}, got {}'
+
+    def __init__(self, identifier, expected, value):
+        AssertionError.__init__(self)
+        ContentHandlerException.__init__(self)
+
+        self.identifier = identifier
+        self.expected_count = expected
+        self.count_value = value
+
+        self.args += (identifier, expected, value)
+
+
+class UnexpectedEmptyInformation(ContentHandlerException, AssertionError):
+    """raised when assertion of non empty array/object fails after parsing"""
+
+    _message_template = '{!r}: information is empty'
+
+    def __init__(self, identifier):
+        AssertionError.__init__(self)
+        ContentHandlerException.__init__(self)
+
+        self.identifier = identifier
+
+        self.args += (identifier,)
