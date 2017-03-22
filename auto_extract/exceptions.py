cc19429d282e3ac9fe5ded2473f565f9eea75d1d
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2017-03-22 13:03:45 +0530

    Adds base exception for TableauPy

--- auto_extract/exceptions.py
+++ auto_extract/exceptions.py
@@ -10,3 +10,19 @@ from __future__ import print_function
 class AutoExtractException(Exception):
     """AutoExtract Command exception"""
     pass
+
+
+class TableauPyException(Exception):
+    """Base TableauPy exception"""
+
+    _message_template = 'A TableauPy exception has occurred'
+
+    def __init__(self, cause=None):
+        Exception.__init__(self)
+        self.cause = cause
+
+    def __str__(self):
+        if self.cause is None:
+            return self._message_template.format(*self.args)
+        else:
+            return str(self.cause)
