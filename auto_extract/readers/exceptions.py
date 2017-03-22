e568f3849216e1bbaf097c2893dc40e136b535b4
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2017-03-22 13:07:20 +0530

    Adds exceptions required by readers

--- auto_extract/readers/exceptions.py
+++ auto_extract/readers/exceptions.py
@@ -1,11 +1,51 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=too-many-ancestors
+
 """Exceptions for Tableau Readers"""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from auto_extract import exceptions
 
-class ReaderException(Exception):
+
+class ReaderException(exceptions.TableauPyException):
     """raised when an exception is thrown by a Reader"""
-    pass
+
+    _message_template = 'An error occurred with Reader'
+
+
+class FileInputException(exceptions.FileIOException, ReaderException):
+    """raised when an exception occurs while reading file for Reader"""
+
+    _message_template = 'An error occurred while reading file: {!r}'
+
+
+class FileNotFound(FileInputException):
+    """raised when input file to be read does not exists"""
+
+    _message_template = '{!r}: file does not exists'
+
+
+class FileNotReadable(FileInputException):
+    """raised when input file to be read does not have read permissions"""
+
+    _message_template = '{!r}: not readable'
+
+
+class NodeNotFile(FileInputException):
+    """raised when input path given as file to be read is not of a file"""
+
+    _message_template = '{!r}: not a file'
+
+
+class FileExtensionMismatch(FileInputException):
+    """raised when input file extension mismatches expected Reader extension"""
+
+    _message_template = '{!r}: does not have extension `{}`'
+
+    def __init__(self, filename, extension):
+        FileInputException.__init__(self, filename=filename)
+        self.extension = extension
+        self.args += (extension,)
