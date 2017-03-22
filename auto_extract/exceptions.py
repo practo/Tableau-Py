bf7af235606bbd2c17b0eb8b1bf6e70a44aa2f2a
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2017-03-22 13:05:54 +0530

    Adds base FileIOException

--- auto_extract/exceptions.py
+++ auto_extract/exceptions.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=too-many-ancestors
+
 """This module defines exceptions for auto_extract package
 """
 
@@ -26,3 +28,17 @@ class TableauPyException(Exception):
             return self._message_template.format(*self.args)
         else:
             return str(self.cause)
+
+
+class FileIOException(TableauPyException, IOError):
+    """Base FileIOException thrown for file input/output exceptions"""
+
+    _message_template = 'An error occurred while reading/writing file: {!r}'
+
+    def __init__(self, filename):
+        IOError.__init__(self)
+        TableauPyException.__init__(self)
+        # using filename attribute reports wrong result
+        # thus using file here
+        self.file = filename
+        self.args += (filename,)
